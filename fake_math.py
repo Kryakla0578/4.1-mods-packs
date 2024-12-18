7f17960ab79b5dde90fc265c595957a85be41fdb
Kryakla0578 <167692954+Kryakla0578@users.noreply.github.com>
2024-12-18 21:08:31 +0300

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,5 @@
+def divide(frst, scnd):
+    if scnd == 0:
+        return 'Ошибка'
+    div = frst/scnd
+    return div
---
+++ true_math.py
@@ -0,0 +1,7 @@
+from math import inf
+
+def divide(frst, scnd):
+    if scnd == 0:
+        return inf
+    div = frst/scnd
+    return div
